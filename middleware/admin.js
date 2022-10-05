3029bc7142cf3574e63e44c140f4e158807ed752
yashajitsaria <ajitsariayash320@gmail.com>
2022-10-06 01:47:14 +0530

    final changes

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,7 @@
-module.exports = function (req, res, next) {
-    if (!req.user.isAdmin) return res.status(403).send('Access denied.');
-    next();
-}
+const config = require("config");
+
+module.exports = function(req, res, next) {
+  if (!config.get("requiresAuth")) return next();
+  if (!req.user.isAdmin) return res.status(403).send("Access denied.");
+  next();
+};
